87802dafb09f8522cbd882332f5bdea0c7098fc4
Ketellin C. <165824547+ketellinjs@users.noreply.github.com>
2025-07-27 15:31:36 -0300

    Create ex0001.py

---
+++ ex0001.py
@@ -0,0 +1,5 @@
+# pede pro usuário digitar o nome e guarda na variável 'nome'
+nome = input('qual seu nome?')
+
+# imprime uma mensagem de boas-vindas ao usuário
+print('oi', nome, 'seja bem vindo!')
